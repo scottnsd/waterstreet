6487f87d7771fb05e050e937692ffccf2e88a199
Terry Sutton <saltcod@gmail.com>
2013-12-10 14:59:17 -0330

    CSS Tricks grid

--- page-grid.php
+++ page-grid.php
@@ -7,49 +7,31 @@
 		<div id="main" class="col12 group" role="main">
 			<div class="entry-content">
 				
-			<h1 class="entry-title">Gridly Adams</h1>
-			
+				<h1 class="entry-title">Gridly Adams</h1>
 
-			<div class='clearfix'>
-				<div class='col1 center keyline-right'>1</div>
-				<div class='col1 center keyline-right'>2</div>
-				<div class='col1 center keyline-right'>3</div>
-				<div class='col1 center keyline-right'>4</div>
-				<div class='col1 center keyline-right'>5</div>
-				<div class='col1 center keyline-right'>6</div>
-				<div class='col1 center keyline-right'>7</div>
-				<div class='col1 center keyline-right'>8</div>
-				<div class='col1 center keyline-right'>9</div>
-				<div class='col1 center keyline-right'>10</div>
-				<div class='col1 center keyline-right'>11</div>
-				<div class='col1 center'>12</div>
-			</div>
+				<div class="row">		 
+					<div class="col-1-4 fill-grey center">1</div>
+					<div class="col-3-4 fill-grey center">3</div>
+				</div>
 
-			<div class="clearfix pad4 keyline-bottom"></div>
+				<div class="row">
+					<div class="col-1-3 fill-grey center">3</div>
+					<div class="col-2-3 fill-grey center">6</div>
+				</div>
 
-			<div class='col12 clearfix pad4y'>
-				<div class='col4 center pad2 fill-grey'>3</div>
-				<div class='col7 center pad2 margin-left1 fill-grey'>9</div>
-			</div>
-			
-			<div class="clearfix pad4 keyline-bottom"></div>
-
-			<div class='col12 clearfix pad4y'>
-				<div class='col5'><img class="size-large" src ="http://666a658c624a3c03a6b2-25cda059d975d2f318c03e90bcf17c40.r92.cf1.rackcdn.com/unsplash_528b276e41166_1.JPG" /></div>
-				<div class='col5 margin-left0'><img class="size-large" src ="http://666a658c624a3c03a6b2-25cda059d975d2f318c03e90bcf17c40.r92.cf1.rackcdn.com/unsplash_528b276e41166_1.JPG" /></div>
-			</div>
-			<div class="clearfix pad4 keyline-bottom"></div>
-
-			<div class='col12 clearfix pad4y'>
-				<div class='col4 pad2 center fill-grey'>3</div>
-				<div class='col4 pad2 center fill-grey dark'>3</div>
-				<div class='col4 pad2 center fill-grey dark'>3</div>
-			</div>
-
-
-
-
-		</div> <!-- .entry-content -->
+				<div class="row">		 
+					<div class="col-1-2 fill-grey center">6</div>
+					<div class="col-1-2 fill-grey center">6</div>
+				</div>
+				
+				<div class="row">		 
+					<div class="col-1-4 fill-grey center">4</div>
+					<div class="col-1-4 fill-grey center">4</div>
+					<div class="col-1-4 fill-grey center">4</div>
+					<div class="col-1-4 fill-grey center">4</div>
+				</div>
+ 
+			</div> <!-- .entry-content -->
 		</div> <!-- #main -->
 	</div> <!-- #inner-content -->  
 </div> <!-- #content -->  
--- page-home.php
+++ page-home.php
@@ -9,47 +9,64 @@
 			<h1 class="entry-title">The Beginning</h1>
 			<div class="entry-content">
 				
-				<div class="group margin4y keyline-bottom">
-					<div class="col6">
+				<div class="row keyline-bottom">
+					<div class="col-1-2">
 						<img class="size-large" src ="https://exposure-3.imgix.net/production/posts/1246/cover-photo/cover-1385733820.jpg" />
 					</div>
 
-					<div class="col5 margin-left0">
+					<div class="col-1-2 ">
 						<img class="size-large" src ="http://666a658c624a3c03a6b2-25cda059d975d2f318c03e90bcf17c40.r92.cf1.rackcdn.com/unsplash_528ef22a4cd0b_1.JPG" />
 						<img class="size-large" src ="http://666a658c624a3c03a6b2-25cda059d975d2f318c03e90bcf17c40.r92.cf1.rackcdn.com/unsplash_528e297be299b_1.JPG" />
-						<p>The first Chauffeur was Bill, a common fellow, as I said before," the  old man expounded; "but his wife was a lady, a great lady. Before the  Scarlet Death she was the wife of Van Worden. He was President of the  Board of Industrial Magnates, and was one of the dozen men who  ruled America. </p>
 					</div>
 				</div>
 
+				<div class="row keyline-bottom">
+					<p>The first Chauffeur was Bill, a common fellow, as I said before," the  old man expounded; "but his wife was a lady, a great lady. Before the  Scarlet Death she was the wife of Van Worden. He was President of the  Board of Industrial Magnates, and was one of the dozen men who  ruled America. </p>
+				</div>		
 
-				<img class="size-large" src ="http://666a658c624a3c03a6b2-25cda059d975d2f318c03e90bcf17c40.r92.cf1.rackcdn.com/unsplash_528c2ebf28f7c_1.JPG" />
 
-				<div class="group margin4y keyline-bottom">
+ 
+				<div class="row keyline-bottom">
 					<h2>Not the first time</h2>
-					<div class="col6 pad1x">
+					<div class="col-1-2">
+						<img class="size-large" src ="http://666a658c624a3c03a6b2-25cda059d975d2f318c03e90bcf17c40.r92.cf1.rackcdn.com/unsplash_528cba6de78e5_1.JPG" />
 						<img class="size-large" src ="http://666a658c624a3c03a6b2-25cda059d975d2f318c03e90bcf17c40.r92.cf1.rackcdn.com/unsplash_528cba6de78e5_1.JPG" />
 						<img class="size-large" src ="http://666a658c624a3c03a6b2-25cda059d975d2f318c03e90bcf17c40.r92.cf1.rackcdn.com/unsplash_528cba6de78e5_1.JPG" />
 					</div>
 					
-					<div class="col6 pad1x">
-						<img class="size-large" src ="http://666a658c624a3c03a6b2-25cda059d975d2f318c03e90bcf17c40.r92.cf1.rackcdn.com/unsplash_528cba6de78e5_1.JPG" />
-						<img class="size-large" src ="http://666a658c624a3c03a6b2-25cda059d975d2f318c03e90bcf17c40.r92.cf1.rackcdn.com/unsplash_528cba6de78e5_1.JPG" />
+					<div class="col-1-2">
+						<p>The first Chauffeur was Bill, a common fellow, as I said before," the  old man expounded; "but his wife was a lady, a great lady. Before the  Scarlet Death she was the wife of Van Worden. He was President of the  Board of Industrial Magnates, and was one of the dozen men who  ruled America. </p>
+						<blockquote>A common fellow, as I said before," the  old man expounded; "but his wife was a lady, a great lady. Before the  Scarlet Death she was the wife of Van Worden. He was President of the  Board of Industrial Magnates, and was one of the dozen men who  ruled America. </blockquote>
 					</div>
 				</div>
 
-				<div class="group col12 ">
-					<div class="col6 margin-left3">
+				<div class="row keyline-all pad4y">
+					<div class="col-1-4">
+						<img class="size-large" src ="http://placehold.it/350x150" />
+						<blockquote>A common fellow, as I said before," the  old man expounded</blockquote>
+					</div>
+					<div class="col-1-4">
+						<img class="size-large" src ="http://placehold.it/350x150" />
+						<blockquote>A common fellow, as I said before," the  old man expounded</blockquote>
+					</div>
+					<div class="col-1-4">
+						<img class="size-large" src ="http://placehold.it/350x150" />
+						<blockquote>A common fellow, as I said before," the  old man expounded</blockquote>
+					</div>
+					<div class="col-1-4">
+						<img class="size-large" src ="http://placehold.it/350x150" />
+						<blockquote>A common fellow, as I said before," the  old man expounded</blockquote>
+					</div>
 						
-						<img class="size-large" src ="http://666a658c624a3c03a6b2-25cda059d975d2f318c03e90bcf17c40.r92.cf1.rackcdn.com/unsplash_528b276e41166_1.JPG" />
-					</div>
+
 				</div>
 
-				<div class="group col12 ">
-					<blockquote class="col4 margin-left4">"Going to string 'em," was the response.</blockquote>
+				<div class="row">
+					<blockquote class="center pad4y">Hoo-Hoo, lying on his stomach and idly digging his toes in the sand,  cried out and investigated, first, his toe-nail, and next, the small  hole he had dug. The other two boys joined him, excavating the sand  rapidly with their hands till there lay three skeletons exposed. Two  were of adults, the third being that of a part-grown child. The old man  hudged along on the ground and peered at the find.</blockquote>
 				</div>
 
 				<div class="col12 group two-col">
-				
+
 					<p>Hoo-Hoo, lying on his stomach and idly digging his toes in the sand,  cried out and investigated, first, his toe-nail, and next, the small  hole he had dug. The other two boys joined him, excavating the sand  rapidly with their hands till there lay three skeletons exposed. Two  were of adults, the third being that of a part-grown child. The old man  hudged along on the ground and peered at the find.</p>
 
 					<p>"Plague victims," he announced. "That's the way they died everywhere  in the last days. This must have been a family, running away from the  contagion and perishing here on the Cliff House beach. They&mdash;what are  you doing, Edwin?"</p>
