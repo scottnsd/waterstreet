711d96ed01389167e062a847b1ba8a328c48673d
Terry Sutton <saltcod@gmail.com>
2013-12-11 16:53:33 -0330

    #logo > #site-title

--- header.php
+++ header.php
@@ -48,7 +48,7 @@
 					<div id="inner-header" class="wrap group">
 
 						<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-						<h1 id="logo"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></h1>
+						<h1 id="site-title"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></h1>
 
 						<?php // if you'd like to use the site description you can un-comment it below ?>
 						<?php // bloginfo('description'); ?>
